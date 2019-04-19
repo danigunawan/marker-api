1930e17c9a922b52981d424075e6b4852ed68da8
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-19 15:58:27 +0530

    Add new error classes and handlers to ExceptionHandler

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,13 +2,30 @@ module ExceptionHandler
   # provides the more graceful `included` method
   extend ActiveSupport::Concern
 
+  # Define custom error subclasses - rescue catches `StandardErrors`
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
       render json: { message: e.message }, status: :not_found
     end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      render json: { message: e.message }, status: :unprocessable_entity
-    end
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_request
+
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :unprocessable_request
+    rescue_from ExceptionHandler::InvalidToken, with: :unprocessable_request
+  end
+
+  private
+
+  def unprocessable_request(e)
+    render json: { message: e.message }, status: :unprocessable_entity
+  end
+
+  def unauthorized_request(e)
+    render json: { message: e.message }, status: :unauthorized
   end
 end
