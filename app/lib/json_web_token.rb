5005b37b862964c7cd485a3169536b95ebef628f
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-19 15:57:56 +0530

    Write new JWT singleton

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,24 @@
+class JsonWebToken
+  # secret to encode and decode token
+  SHARED_SECRET = Rails.application.secrets[:jwt_shared_secret]
+
+  # TODO:
+  # Currently a static encoded access token (generated once) is shared with the client.
+  # This can easily be scaled to generated on-demand tokens with user-specific payloads
+  # and an expiry
+  def self.encode(payload = { client: :reactnative })
+    # sign token with a secret shared between clients and API server
+    byebug
+    JWT.encode(payload, SHARED_SECRET)
+  end
+
+  def self.decode(access_token)
+    # get payload; first index in decoded Array
+    body = JWT.decode(access_token, SHARED_SECRET)[0]
+    HashWithIndifferentAccess.new body
+    # rescue from all decode errors
+  rescue JWT::DecodeError => e
+    # raise custom error to be handled by custom handler
+    raise ExceptionHandler::InvalidToken, e.message
+  end
+end
