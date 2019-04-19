22c0040a947e8cb0a9e748aba47b7ddc10f6025e
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-19 16:45:26 +0530

    green: implement authorize_request in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
   include ExceptionHandler
+
+  before_action :authorize_request
+
+  private
+
+  def authorize_request
+    AuthorizeApiRequest.new(request.headers).call
+  end
 end
