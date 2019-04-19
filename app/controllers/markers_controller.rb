e15b6f5687a2b647f0c9e3c868892c6bf3f5e451
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-19 14:42:57 +0530

    Add controller methods

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -1,2 +1,28 @@
 class MarkersController < ApplicationController
+  before_action :set_marker, only: [:destroy]
+
+  def index
+    @markers = Marker.all
+    render json: @markers, status: :ok
+  end
+
+  def create
+    @marker = Marker.create!(marker_params)
+    render json: @marker, status: :created
+  end
+
+  def destroy
+    @marker.destroy
+    head :no_content
+  end
+
+  private
+
+  def marker_params
+    params.permit(:lat, :long)
+  end
+
+  def set_marker
+    @marker = Marker.find(params[:id])
+  end
 end
