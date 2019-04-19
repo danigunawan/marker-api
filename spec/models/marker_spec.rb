da32609b8a82a4e5ba24f4128dfe333d96fdea25
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-19 13:39:33 +0530

    red: Marker should validate fields

--- spec/models/marker_spec.rb
+++ spec/models/marker_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Marker, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:lat) }
+  it { should validate_presence_of(:long) }
 end
