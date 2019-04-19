edc710194169b5eb5e5721d11cb0e9dc2b21d0ba
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-19 18:07:45 +0530

    Use PostgreSQL instead of SQLite for enabling Heroku deployment

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,1 +1,1 @@
-Dotenv.require_keys("JWT_SHARED_SECRET")
+Dotenv.require_keys("JWT_SHARED_SECRET", "MARKER_DATABASE_PASSWORD")
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_18_221118) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "markers", force: :cascade do |t|
     t.decimal "lat"
     t.decimal "long"
